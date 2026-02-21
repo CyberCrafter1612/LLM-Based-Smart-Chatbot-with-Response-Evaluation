d76f7b2ba8793975fdc084801f099e688d4c339a
Sumit Kumar <oreojin1514@gmail.com>
2026-02-22 00:55:34 +0530

    Create prompt_refiner.py

---
+++ prompt_refiner.py
@@ -0,0 +1,8 @@
+def refine_prompt(query):
+    return f"""
+    Improve the following query to get a more accurate, clear and detailed answer:
+
+    Original Query: {query}
+
+    Improved Query:
+    """
